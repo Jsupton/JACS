32851b1f2aa21e77a27ef630a471a76e7ca30d85
CJZINS <cjzins@viewlincsci30.ad.csbsju.edu>
2017-02-26 17:27:55 -0600

    casey added the adminUI class

---
+++ src/AdminUI.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package JACS;
+
+/**
+ * @author cjzins
+ *
+ */
+public class AdminUI {
+	/**
+	 * a variable for admin
+	 */
+	Admin admin;
+	/**
+	 * a method to get the univiersities
+	 * @return an array list of the universitys
+	 */
+	public ArrayList getUniversities(){
+		ArrayList<University> u = ArrayList();
+		return u;
+	}
+	/**
+	 * displays the universities
+	 */
+	public void displayUniversities(){
+		
+	}
+	/**
+	 * a method to display the accounts
+	 * @return an array list of accounts
+	 */
+	public ArrayList<Account> displayAccounts(){
+		ArrayList<Account> u = ArrayList();
+		return a;
+	}
+	/**
+	 * adds a university to the database
+	 * @param an university that is being added
+	 */
+	public void addUniversity(University u){
+		
+	}
+	/**
+	 * gets the search error
+	 * @return a String representing the error
+	 */
+	public String getSearchError(){
+		
+	}
+	}
+}

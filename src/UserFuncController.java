86c7899678fa6edef2d58c055fc959a32083343d
saschmidgall <saschmidgall@viewlincsci25.ad.csbsju.edu>
2017-02-26 19:04:29 -0600

    adding UserFuncController

---
+++ src/UserFuncController.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+
+/**
+ * 
+ */
+
+/**
+ * @author saschmidgall
+ *
+ */
+public class UserFuncController {
+
+	/**
+	 * 
+	 */
+	User u;
+	public UserFuncController() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	public UserFuncController(User u) {
+		super();
+		this.u = u;
+	}
+	
+	public void addUniversityToSavedSchools(University uni, User u)
+	{
+		
+	}
+	
+	public void removeUniversityFromSavedSchools(University uni, User u)
+	{
+		
+	}
+	
+	public ArrayList<University> searchForSchools(University uni)
+	{
+		return null;
+	}
+	
+	public void viewUniversityDetails(University u)
+	{
+		
+	}
+	
+	public void viewStudentProfile(User u)
+	{
+		
+	}
+		
+	public void editStudentProfile(User u)
+	{
+		
+	}
+}	

1b6e3d13ea686bc5a170546414294836e2ed5a45
jsupton <jsupton@viewlincsci08.ad.csbsju.edu>
2017-02-26 20:08:10 -0600

    Small Fixes

--- src/edu/csbsju/Admin.java
+++ src/edu/csbsju/Admin.java
@@ -4,28 +4,53 @@
 package edu.csbsju;
 
 /**
+ * This class acts as an Administrator object. They are able to perform
+ * different functionalities than an User can, thus this class allows 
+ * Adminitrator to perform those action.
  * @author cjzins
- *
+ * @version 02-26-17
  */
-public class Admin {
+public class Admin extends Account{
 
- /**
-  * a constructor that creates an admin
-  */
- public Admin() {
-  // TODO Auto-generated constructor stub
- }
- /**
-  * a method to display all of the accounts
-  */
- public void displayAccounts(){
+	
+	/**
+	 * this constructor creates an Admin object using the constructor
+	 * from Account, which Admin extends
+	 * @param firstName the name of an admin
+	 * @param lastName the lastname of an admin
+	 * @param username the admin's username
+	 * @param password the admin's password
+	 * @param status the status of the admin
+	 * @param type the type of the admin
+	 */
+	public Admin(String firstName, String lastName, String username, String password, char status, char type) {
+		super(firstName, lastName, username, password, status, type);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * this constructor creates an Admin object using the constructor
+	 * from Account, which Admin extends
+	 * @param firstName the name of an admin
+	 */
+	public Admin(String firstName) {
+		super(firstName);
+		// TODO Auto-generated constructor stub
+	}
+
+
+	/**
+	 * a method to display all of the accounts
+	 */
+	public void displayAccounts(){
   
- }
- /**
-  * displays all of the universities
-  */
- public void displayUniversities(){
+	}
+	
+	/**
+	 * displays all of the universities
+	 */
+	public void displayUniversities(){
   
- }
+	}
 
 }
--- src/edu/csbsju/AdminUI.java
+++ src/edu/csbsju/AdminUI.java
@@ -1,52 +1,63 @@
-/**
- * 
- */
 package edu.csbsju;
 
 import java.util.*;
 
 /**
+ * This class is the UserInterface for an Administrator. This class
+ * controls what is displayed to the user at specific points in time.
+ * The controller will send data back to this class to display to the Admin.
  * @author cjzins
- *
+ * @version 02-26-17
  */
 public class AdminUI {
- /**
-  * a variable for admin
-  */
- Admin admin;
- /**
-  * a method to get the univiersities
-  * @return an array list of the universitys
-  */
- public ArrayList<Integer> getUniversities(){
-  return null;
- }
- /**
-  * displays the universities
-  */
- public void displayUniversities(){
-  
- }
- /**
-  * a method to display the accounts
-  * @return an array list of accounts
-  */
- public ArrayList<Account> displayAccounts(){
-  return null;
- }
- /**
-  * adds a university to the database
-  * @param an university that is being added
-  */
- public void addUniversity(University u){
-  
- }
- /**
-  * gets the search error
-  * @return a String representing the error
-  */
- public String getSearchError(){
-   return null;
- }
- }
 
+	//a variable for admin
+	Admin admin;
+	
+	/**
+	 * This method is used to get all the universities that reside in the database.
+	 * This method will call the AdminFuncController.
+	 * @return an array list of the universitys
+	 */
+	public ArrayList<University> getUniversities(){
+		ArrayList<University> u = new ArrayList();
+		return u;
+	 }
+	 
+	/**
+	  * This method is responsible for displaying all of the universities
+	  * to the Admin.
+	  */
+	 public void displayUniversities(){
+	  
+	 }
+	 
+	 /**
+	  * This is a method used to display the accounts of all of the Account
+	  * objects that are in the database.
+	  * @return an array list of accounts
+	  */
+	 public ArrayList<Account> displayAccounts(){
+		 ArrayList<Account> a = new ArrayList();
+	  return a;
+	 }
+	 
+	 /**
+	  * This method is responsible for adding a specific university to the 
+	  * database. It takes a university and updates the database.
+	  * @param an university that is being added
+	  */
+	 public void addUniversity(University u){
+	  
+	 }
+	 
+	 /**
+	  * This method displays an error method. It is used when there is
+	  * an error when searching the database.
+	  * @return a String representing the error
+	  */
+	 public String getSearchError(){
+	   return null;
+	 }
+}
+	

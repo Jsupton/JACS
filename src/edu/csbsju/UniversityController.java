1c02ca0e63f4b45b56adc2d6207aa30e25405d21
jsupton <jsupton@viewlincsci08.ad.csbsju.edu>
2017-02-26 19:20:55 -0600

    moved UniversityController to Package

---
+++ src/edu/csbsju/UniversityController.java
@@ -0,0 +1,31 @@
+package edu.csbsju;
+
+public class UniversityController {
+	private University university;
+	
+	/**
+	 * Adds a university to the database
+	 * @param u
+	 */
+	public void addUniversity(University u)
+	{
+
+	}
+	/**
+	 * 
+	 * @param u
+	 */
+	public void editUniversity(University u)
+	{
+		
+	}
+	/**
+	 * 
+	 * @param u
+	 */
+	public void displayUniversity(University u)
+	{
+		
+	}
+
+}

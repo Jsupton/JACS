e8ea5d587e854271750db038835e2b21967f86eb
saschmidgall <saschmidgall@viewlincsci25.ad.csbsju.edu>
2017-02-26 19:24:17 -0600

    adding UserUI

---
+++ src/edu/csbsju/UserUI.java
@@ -0,0 +1,70 @@
+/**
+ * 
+ */
+package edu.csbsju;
+
+import java.util.ArrayList;
+
+/**
+ * @author saschmidgall
+ *
+ */
+public class UserUI {
+
+	/**
+	 * 
+	 */
+	public UserUI() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void logon(String u, String p)
+	{
+		
+	}
+	
+	public User getStudentProfile()
+	{
+		return null;	
+	}
+
+	public void displayStudentDetails(String name)
+	{
+		
+	}
+	
+	public ArrayList<University> search(User u)
+	{
+		return null;
+	}
+	
+	public ArrayList<University> getSavedSchools(User u)
+	{
+		return null;
+	}
+	
+	public ArrayList<University> displaySearchedUniversities(ArrayList<University> alu)
+	{
+		return null;
+	}
+	
+	public String getSearchError()
+	{
+		return null;
+	}
+	
+	public void editStudentProfile(User u)
+	{
+		
+	}
+	
+	public void displaySavedSchoolsError()
+	{
+		
+	}
+	
+	public void saveUserSchool(User u, University uni)
+	{
+	
+	}
+}
